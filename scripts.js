6b7b62092d3b20c928fb9be21568b7dd81c35412
kovacsbenc3 <73656785+kovacsbenc3@users.noreply.github.com>
2020-12-16 16:32:48 +0100

    Ex 1

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let color = "purple";
+let number = 10;
+let word = "cool";
+
+if (color === "purple") {
+$(".n").css("background", "purple");
+}
+
+if (number > 100) {
+    $(".e").text("Whoah,thats BIG number");
+}else{
+    $(".e").text("just a regular number, pls");
+}
+
+if (word === "cool") {
+    $(".w").text("Power fo DOM");    
+}else{
+    $(".s").text("Power of DOM");
+}
+
+
+
